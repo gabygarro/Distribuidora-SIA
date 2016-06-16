e01db22368a17dadc7488fb634d782352411970d
Gabriela Garro Abdykerimov <ggarroab@gmail.com>
2016-06-15 22:17:45 -0600

    Added order functionality

--- Interfaz/admin/pages/pedido.php
+++ Interfaz/admin/pages/pedido.php
@@ -15,6 +15,34 @@
         header("Location: ../../index.php#notadmin");
     }
 
+    //if the form was sent
+    if(isset($_POST["pedido"])) {
+        $cliente = $_POST["cliente"];
+        $bodega = $_POST["bodega"];
+        $idPedido = 0;
+
+        $queryPedido = mysqli_query($conn, "INSERT INTO Pedido (Cliente_IdCliente, entregado) VALUES ('$cliente', 0);");
+
+        $queryLastInsert = mysqli_query($conn, "SELECT LAST_INSERT_ID();");
+        $numrows = mysqli_num_rows($queryLastInsert);
+        if ($numrows != 0) {
+            while ($row = mysqli_fetch_assoc($queryLastInsert)) {
+                $idPedido = $row['LAST_INSERT_ID()'];
+            }
+            for ($i = 0; isset($_POST['producto' . $i]); $i++) {
+                $currentProducto = $_POST['producto' . $i];
+                $currentCantidad = $_POST['cantidad' . $i];
+                $queryInventario = mysqli_query($conn, "UPDATE Inventario SET cantidad = cantidad - '$currentCantidad' WHERE Bodega_idBodega = '$bodega' AND Producto_idProducto = '$currentProducto' AND cantidad > 0;");
+
+                $queryInsertProducto = mysqli_query($conn, "INSERT INTO ArticulosXPedido (Inventario_Bodega_idBodega, Inventario_Producto_idProducto, Pedido_idPedido, cantidad) VALUES ('$bodega', '$currentProducto', '$idPedido', '$currentCantidad');");
+            }
+
+        }
+        else {
+            echo "Last Insert devolvió 0 filas.";
+        }
+
+    }
 
 
 ?>
@@ -62,13 +90,76 @@
     <script>
         // Clientes
         var arrayClientes = <?php echo json_encode($_SESSION['arrayClientes'], JSON_PRETTY_PRINT) ?>;
-        window.onload = function () {
+        //Productos e Inventario
+        var arrayProductos = <?php echo json_encode($_SESSION['arrayProductos'], JSON_PRETTY_PRINT) ?>;
+        var arrayInventario = <?php echo json_encode($_SESSION['arrayInventario'], JSON_PRETTY_PRINT) ?>; 
+        var numPedidos = 0;
+
+        function populateProductos(productoID, cantidadID, bodegaID) {
+            var elementProducto = document.getElementById(productoID);
+            var elementBodega = document.getElementById(bodegaID);
+            elementProducto.length = 0;
+            elementProducto.options[0] = new Option("Seleccione producto", "-1");
+            elementProducto.selectedIndex = 0;
+            for (var i = 0; i <= Object.keys(arrayInventario[elementBodega.value]).length; i++) {
+                if (typeof arrayInventario[elementBodega.value][i.toString()] != 'undefined') {
+                    var idProductoXBodega = arrayInventario[elementBodega.value][i.toString()]["Producto_idProducto"];
+                    //console.log("idProductoXBodega = " + idProductoXBodega);
+                    var optionNombre = arrayProductos[idProductoXBodega]["ProductoNombre"];
+                    optionNombre += " (";
+                    optionNombre += arrayProductos[idProductoXBodega]["descripcion"];
+                    optionNombre += ")";
+                    var optionID = arrayProductos[idProductoXBodega]["idProducto"];
+                    elementProducto.options[elementProducto.length] = new Option(optionNombre, optionID);
+                }
+            }
+
+            elementProducto.onchange = function(){
+                var producto = elementProducto.value;
+                var elementCantidad = document.getElementById(cantidadID);
+                var cantidad = arrayInventario[elementBodega.value][producto]['cantidad'];
+                //console.log(cantidad);
+                elementCantidad.max = cantidad;
+            };
+        } 
+
+        function anadirProducto(divID) {
+            numPedidos++;
+            var code = "<div class=\"row\">\n    <div class=\"col-md-8\">\n";
+            code += "        <br><select name=\"producto";
+            code += numPedidos + "\" id=\"producto";
+            code += numPedidos + "\" class=\"form-control\"></select>\n";
+            code += "    </div>\n    <div class=\"col-md-2\">";
+            code += "        <br><input type=\"number\" min=\"0\" step=\"1\" name=\"cantidad";
+            code += numPedidos + "\" id=\"cantidad";
+            code += numPedidos + "\" class=\"form-control\">    </div>\n";
+            code += "    <div class=\"col-md-2\">";
+            code += "        <br><button class=\"btn btn-default\" onclick=\"return false;\"><i class=\"fa fa-times fa-fw\"></i></button>";
+            code += "    </div>\n</div>";
+            var div = document.getElementById(divID);
+            div.insertAdjacentHTML('beforeend', code);
+            populateProductos('producto' + numPedidos, 'cantidad' + numPedidos, 'bodega');
+        }
+
+        window.onload = function() {
             var elementClientes = document.getElementById('cliente');
             elementClientes.length = 0;
             elementClientes.options[0] = new Option('Seleccione el cliente', '-1');
             elementClientes.selectedIndex = 0;
             for (var i = 0; i < arrayClientes.length; i++) {
                 elementClientes.options[elementClientes.length] = new Option(arrayClientes[i]["nombreLocal"], arrayClientes[i]["idCliente"]);
+            }
+            elementClientes.onchange = function() {
+                var elementBodega = document.getElementById('bodega');
+                elementBodega.value = "-1";
+                for (var i = 0; i < arrayClientes.length; i++) {
+                    if (elementClientes.options[elementClientes.selectedIndex].value == arrayClientes[i]["idCliente"]) {
+                        elementBodega.value = arrayClientes[i]["bodega"];
+                        //break;
+                    }
+                }
+                //populateProductos
+                populateProductos('producto0', 'cantidad0', 'bodega');
             }
         };
     </script>
@@ -320,8 +411,26 @@
                 <!-- /.col-lg-12 -->
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <label>Cliente</label>
-                        <select id="cliente" name ="cliente" class="form-control"></select>
+                        <form role="form" name="pedido" action="pedido.php" method="POST">
+                            <label>Cliente</label>
+                            <select id="cliente" name ="cliente" class="form-control"></select>
+                            <input id="bodega" name="bodega" type="hidden" value="-1"></input>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <label>Productos</label>
+                                    <select name="producto0" id="producto0" class="form-control"></select>
+                                </div>
+                                <div class="col-md-2">
+                                    <label>Cantidad</label>
+                                    <input type="number" min="1" step="1" name="cantidad0" id="cantidad0" class="form-control">
+                                </div>
+                            </div>
+                            <div id="masproductos"></div>
+                            <br>
+                            <button class="btn btn-default" id="otroProducto" onclick="anadirProducto('masproductos');return false;">Añadir producto</button>
+                            <button name="pedido" class="btn btn-primary" type="submit">Hacer pedido</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- Interfaz/admin/pages/session.php
+++ Interfaz/admin/pages/session.php
@@ -6,6 +6,8 @@
     * session.php - Creado: 13/06/16
     * Control de la sesión actual (admin)
     */
+
+   header('Content-type: text/html; charset=utf-8');
 	
 	// Connect to the db
 	$dbhost = "localhost";
@@ -16,6 +18,8 @@
 
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die($dberror);
 
+	mysqli_set_charset($conn, "UTF8");
+
 	if ($conn == true) {
 		session_start();
 		if (isset($_SESSION['usernameID'])) { // El usuario sí inició sesión
@@ -23,15 +27,33 @@
 			$username = $_SESSION['username'];
 			$userType = $_SESSION['userType'];
 
+			// Obtener el id de Cliente, su nombre y la bodega a la que está asociado
 			$queryClientes = mysqli_query($conn, "SELECT idCliente, nombreLocal, Bodega_idBodega as bodega FROM cliente, clienteXRuta, Ruta WHERE idCliente = Cliente_idCliente AND Ruta_idRuta = idRuta;");
 			$clientes = mysqli_num_rows($queryClientes);
-
 			$arrayClientes = array();
 			while($row = mysqli_fetch_assoc($queryClientes)) {
 				$arrayClientes[] = $row;
 			}
 			$_SESSION['arrayClientes'] = $arrayClientes;
 
+			// Obtener el catálogo de productos
+			$queryProductos = mysqli_query($conn, "SELECT idProducto, Producto.nombre as ProductoNombre, descripcion, precioCompra, precioVenta, impuesto, pesoPorCaja, Categoria.nombre as CategoriaNombre, Marca.nombre as MarcaNombre FROM Producto, Categoria, Marca WHERE Categoria_idCategoria=idCategoria AND Marca_idMarca = idMarca;");
+			$productos = mysqli_num_rows($queryProductos);
+			$arrayProductos = array();
+			while ($row = mysqli_fetch_assoc($queryProductos)) {
+				$arrayProductos[$row['idProducto']] = $row;
+			}
+			$_SESSION['arrayProductos'] = $arrayProductos;
+
+			// Obtener el inventario de cada bodega
+			$queryInventario = mysqli_query($conn, "SELECT * from Inventario;");
+			$arrayInventario = array();
+			while ($row = mysqli_fetch_assoc($queryInventario)) {
+				if (0 < $row['cantidad']) {
+					$arrayInventario[$row['Bodega_idBodega']][$row['Producto_idProducto']] = $row;
+				}
+			}
+			$_SESSION['arrayInventario'] = $arrayInventario;
     	}
     	else {
     		echo "No inició sesión.";
